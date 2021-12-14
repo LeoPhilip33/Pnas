9121a44fc559f5df2024c8d9f53f57916bc8c15e
LeoPhilip33 <leo.philip33.bordeaux@gmail.com>
2021-12-14 22:21:47 +0100

    push save last version

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:async';
+
+var url = Uri.parse('http://192.168.0.31/on-off.php');
+var urlPingServer = Uri.parse('http://192.168.0.31/ping.php');
 
 void main() {
   runApp(const MyApp());
@@ -13,6 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pnas Status',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -30,8 +35,33 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String serverStatus = "Loading...";
+
   @override
   Widget build(BuildContext context) {
+    Future<void> pingServer() async {
+      var response = await http.post(urlPingServer);
+      if (response.statusCode == 200) {
+        var bodyPingServer = response.body;
+
+        if (bodyPingServer == "true") {
+          setState(() {
+            serverStatus = "ON";
+          });
+        } else {
+          setState(() {
+            serverStatus = "OFF";
+          });
+        }
+      } else {
+        print('Erreur, ping pas envoyé');
+      }
+    }
+
+    Timer.periodic(const Duration(seconds: 2), (timer) {
+      pingServer();
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -40,10 +70,15 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text(
+              'Serveur Status : $serverStatus',
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
             TextButton(
               onPressed: () {
-                _launchURL();
-                debugPrint("Data send");
+                _lunchServer();
               },
               child: const Text('Démarer le serveur'),
             ),
@@ -54,14 +89,12 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-var url = Uri.parse('http://192.168.0.31/');
-
-void _launchURL() async {
+void _lunchServer() async {
   var response = await http.post(url);
   if (response.statusCode == 200) {
-    print('Requete bien envoyé');
+    print('BOOT bien envoyé');
+    print('Response body: ${response.body}');
   } else {
     print('Erreur, veuillez me contacter a : leo.philip33.bordeaux@gmail.com');
   }
-  print('Response body: ${response.body}');
 }
